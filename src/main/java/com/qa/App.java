1179f855a1b945b814bfc6bd747e44f461ac58d0
ziya <ziyameng123@gmail.com>
2024-11-04 14:26:16 +0000

    added ziya

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Ziya"));
         System.out.println(sayGoodbye());
     }
 
